15723ff8ee73c9ccd9a793d7f84393f2798719cf
Alan Thornburg <alan.thornb@gmail.com>
2017-08-08 18:59:36 -0400

    Updated gulpfile to use browsersync to serve the app.  To do:  set up source maps

--- regex-library/gulpfile.js
+++ regex-library/gulpfile.js
@@ -1,16 +1,22 @@
 var browserify = require('browserify');
+var browserSync = require('browser-sync');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 
 gulp.task('scripts', function() {
-    return browserify({ entries: ['./src/app.js'] })
+    return browserify({ entries: ['./src/js/app.js'] })
         .bundle()
         .pipe(source('bundle.js'))
-        .pipe(gulp.dest('./.tmp'))
+        .pipe(gulp.dest('.tmp'))
 });
 
 //gulp.task('watch', )
 
-//gulp.task('serve', ['watch'])
+gulp.task('serve', ['scripts'], function() {
+    browserSync.init({
+        // serve from the src/ and .tmp/ directories
+        server: ["src", ".tmp"]
+    })
+});
 
-gulp.task('default', ['scripts']);
+gulp.task('default', ['serve']);
