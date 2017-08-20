819612d51d126186a2e5e65ba77e4d78ff65a2aa
aathornburg <alan.thornb@gmail.com>
2017-08-20 18:48:04 -0400

    Added nav bar and search bar component

--- RegexLibrary/src/app/app.module.ts
+++ RegexLibrary/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBoxComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule
