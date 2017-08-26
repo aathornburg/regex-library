cc5288fa386177d65f68b7d4eddf6f697473f54c
aathornburg <alan.thornb@gmail.com>
2017-08-26 19:07:52 -0400

    Updated routing for nav links to work correctly

--- RegexLibrary/src/app/app.routing.ts
+++ RegexLibrary/src/app/app.routing.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SearchBoxComponent } from './search-box/search-box.component';
 
 const routes: Routes = [
-    { path: 'home', component: SearchBoxComponent },
+    { path: '', component: SearchBoxComponent },
     { path: 'search', component: SearchBoxComponent },
     { path: 'browse', component: SearchBoxComponent }
   ];
