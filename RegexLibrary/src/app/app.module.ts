08de4961a2044cd714b84a11044a00846416f7fe
aathornburg <alan.thornb@gmail.com>
2018-02-11 13:58:00 -0500

    Changed search box styles and began start of turning search bar into a form

--- RegexLibrary/src/app/app.module.ts
+++ RegexLibrary/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SearchBoxComponent } from './search-box/search-box.component';
@@ -18,6 +19,7 @@ import { routing } from './app.routing';
   imports: [
     BrowserModule,
     RouterModule,
+    FormsModule,
     routing
   ],
   providers: [],
